5e7f0fb806f7001d9a926ce1137daea8ba6ca71f
Turgay <turgayaltunalan@mail.com>
2026-02-25 13:34:34 +0000

    README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+# Spacewalks
+
+## Overview
+Spacewalks is a python analysis tool for generating 
+## Features
+Key features of Spacewalks:
+
+-Generates a CSV table of summary statistics of extravehicular activity and crew sizes
+-Generates a time point to show the cumulative over time
+## Pre-requisits
+Spacewalks was developed using Python 3.12
+
+To install and run Spacewalks you will need to have Python >=3.12 installed. You will also need the following libraries:
+
+- [Numpy] (https://www.numpy.org/)
+- [Matplotlib] (https://matplotlib.org/Stable/index.html)
+- [pytest] (https://docs.pytest.org/)
+- [pandas] (htpps://pandas.org/)
+## Installation instructions
+Clone the repository to your local machine to using git:
+
+`git clone git@github.com:turgayalt/spacewalks.git`
+
+Create a virtual environment:
+`python -m venv venv_spacewalks`
--- eva_data_analyis.py
+++ eva_data_analyis.py
@@ -1,61 +1,100 @@
-# https://data.nasa.gov/resource/eva.json (with modifications)
-data_f = open('./eva-data.json', 'r', encoding='ascii')
-data_t = open('./eva-data.csv','w', encoding='utf-8')
-g_file = './cumulative_eva_graph.png'
-
-fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
-
-data=[]
-import json
-
-for i in range(375):
-    line=data_f.readline()
-    print(line)
-    data.append(json.loads(line[1:-1]))
-#data.pop(0)
-## Comment out this bit if you don't want the spreadsheet
-import csv
-
-w=csv.writer(data_t)
-
-import datetime as dt
-
-time = []
-date =[]
-
-j=0
-for i in data:
-    print(data[j])
-    # and this bit
-    w.writerow(data[j].values())
-    if 'duration' in data[j].keys():
-        tt=data[j]['duration']
-        if tt == '':
-            pass
-        else:
-            t=dt.datetime.strptime(tt,'%H:%M')
-            ttt = dt.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second).total_seconds()/(60*60)
-            print(t,ttt)
-            time.append(ttt)
-            if 'date' in data[j].keys():
-                date.append(dt.datetime.strptime(data[j]['date'][0:10], '%Y-%m-%d'))
-                #date.append(data[j]['date'][0:10])
-
-            else:
-                time.pop(0)
-    j+=1
-
-t=[0]
-for i in time:
-    t.append(t[-1]+i)
-
-date,time = zip(*sorted(zip(date, time)))
-
 import matplotlib.pyplot as plt
+import pandas as pd
+import sys as pd
+def main(input_file,output_file,graph_file):
+    print("--START--")
+def read_json_to_dataframe(input_file):
+    """
+    Read a json file into a pandas dataframe
+    Args:
+        input_file: String, input file name, expects a json extension
+    Returns:
+        eva_df: pandas dataframe
+    """
+    print(f'Reading JSON file {input_file}')
+    # Read the data from a JSON file into a Pandas dataframe
+    eva_df = pd.read_json(input_file, convert_dates=['date'], encoding='ascii')
+    eva_df['eva'] = eva_df['eva'].astype(float)
+    # Clean the data by removing any rows where duration is missing
+    eva_df.dropna(axis=0, subset=['duration', 'date'], inplace=True)
+    return eva_df
 
-plt.plot(date,t[1:], 'ko-')
-plt.xlabel('Year')
-plt.ylabel('Total time spent in space to date (hours)')
-plt.tight_layout()
-plt.savefig(g_file)
-plt.show()
+def write_dataframe_to_csv(df, output_file):
+    """
+    write dataframe on csv format
+    """
+    print(f'Saving to CSV file {output_file}')
+    # Save dataframe to CSV file for later analysis
+    df.to_csv(output_file, index=False, encoding='utf-8')
+
+def add_duration_hours(df):
+    """
+    Add duration in hours (duration_hours) variable to the the dataset
+    
+    Args:
+        df (pd.Dataframe): the input dataframe
+        
+    Returns:
+    df_copy (pd.Dataframe): A copy of the dataframe with the new duration_hours variable added.
+    """
+df_copy = df.copy()
+df_copy["duration_hours"] =df_copy["duration"].apply(text_to_duration)
+return df_copy
+
+def text_to_duration(df):
+    """
+    Convert a text format duration "HH:MM" to duration in hours
+    
+    Args:
+        duration (str): The text format duration
+        
+        Return:
+            duration_hours (float): The duration in hours
+    """
+    hours, minutes = duration.split(":")
+    duration_hours = int(hours) + int
+def plot_cumulative_time_in_space(df, graph_file):
+    print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
+    df = duration
+    df['cumulative_time'] = df['duration_hours'].cumsum()
+    plt.plot(df['date'], df['cumulative_time'], 'ko-')
+    plt.xlabel('Year')
+    plt.ylabel('Total time spent in space to date (hours)')
+    plt.tight_layout()
+    plt.savefig(graph_file)
+    plt.show()
+
+# Main code
+def main(args):
+    # perform some actions
+    main(args)
+    
+print("--START--")
+
+input_file = open('./eva-data.json', 'r', encoding='ascii')
+output_file = open('./eva-data.csv', 'w', encoding='utf-8')
+graph_file = './cumulative_eva_graph.png'
+
+# Read the data from JSON file
+eva_data = read_json_to_dataframe(input_file)
+
+# Convert and export data to CSV file
+write_dataframe_to_csv(eva_data, output_file)
+
+# Sort dataframe by date ready to be plotted (date values are on x-axis)
+eva_data.sort_values('date', inplace=True)
+
+# Plot cumulative time spent in space over years
+plot_cumulative_time_in_space(eva_data, graph_file)
+
+print("--END--")
+if __name__ == "__main__":
+    if len(sys.argv) <3:
+        input_file = 'eva-data.json'
+        output_file = '.eva-data.csv'
+        print('Using default input and output filenames')
+    else:
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
+        print('Using custom input and output filenames')
+    input_file = open(', 'r', encoding='ascii'))')
